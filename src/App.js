3c56cea6c5be141219b150f1ed6b1956f52dc0c8
Shakil2015 <sshakil496@gmail.com>
2020-08-13 18:05:31 +0600

    office

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from 'react';
+import React,{useState} from 'react';
+import { FormControl,Select,MenuItem } from '@material-ui/core'
+import './App.css'
 
 function App() {
+  const [countries,setCountry] =useState(['Bangladesh','Turky','Plastine','Iran'])
   return (
     <div className="App">
-      
+      <div className='app_header'>  
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl className='app_dropdown'>
+          <Select
+          variant='outlined'
+          value='abc'
+          >
+            {
+              countries.map(country=>(
+              <MenuItem value='WorlWide'>{country}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
